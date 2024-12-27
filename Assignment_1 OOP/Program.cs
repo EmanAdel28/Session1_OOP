fe8b7a6e5649b8c26df044e46c31b2d8cfddfd99
AmiraAdel28 <amira.adel282001@gmail.com>
2024-12-27 17:06:03 +0200

    Q3

--- Assignment_1 OOP/Program.cs
+++ Assignment_1 OOP/Program.cs
@@ -10,6 +10,13 @@
     {
         Spring, Summer, Autumn, Winter
     }
+
+    [Flags]
+    enum Permissions : byte
+    {
+        Delete =1, Execute =2, Read=4 , write=8
+        
+    }
     internal class Program
     {
 
@@ -34,35 +41,71 @@
             //Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
             //Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
             //Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
-            bool Flags;
-            Season season;
-            do
+
+            //bool Flags;
+            //Season season;
+            //do
+            //{
+            //    Console.WriteLine("Enter the season");
+            //    Flags = Enum.TryParse<Season>(Console.ReadLine(), true, out season);
+
+            //} while (!Flags);
+
+            //switch (season)
+            //{
+            // case Season.Spring:
+            //        Console.WriteLine("spring: march to may ");
+            //        break;
+
+            // case Season.Summer:
+            //        Console.WriteLine("summer: june to august");
+            //        break;
+
+            // case Season.Autumn:
+            //        Console.WriteLine("autumn: September to November");
+            //        break;
+
+            // case Season.Winter:
+            //        Console.WriteLine("winter: December to February");
+            //        break;
+
+            //}
+
+            #endregion
+
+            #region Q3
+            //Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum.
+            //Create Variable from previous Enum to Add and Remove Permission from variable,
+            //check if specific Permission is existed inside variable
+
+            Permissions permissions = new Permissions();
+
+            // Add Permision
+            permissions= permissions | Permissions.Delete;
+            permissions = permissions | Permissions.Read;
+            permissions = permissions | Permissions.Execute;
+
+            Console.WriteLine(permissions);
+
+            // Delete 
+            if((permissions & Permissions.Execute) ==  Permissions.Execute)
             {
-                Console.WriteLine("Enter the season");
-                Flags = Enum.TryParse<Season>(Console.ReadLine(), true, out season);
-
-            } while (!Flags);
-
-            switch (season)
-            {
-             case Season.Spring:
-                    Console.WriteLine("spring: march to may ");
-                    break;
-
-             case Season.Summer:
-                    Console.WriteLine("summer: june to august");
-                    break;
-
-             case Season.Autumn:
-                    Console.WriteLine("autumn: September to November");
-                    break;
-
-             case Season.Winter:
-                    Console.WriteLine("winter: December to February");
-                    break;
+                // if exist it will delete
+                permissions = permissions ^ Permissions.Execute;
 
             }
-            
+            else
+            {
+                // if not exist => does not make action 
+                permissions = permissions;
+
+
+            }
+
+            Console.WriteLine(permissions);
+
+
+
             #endregion
         }
     }
